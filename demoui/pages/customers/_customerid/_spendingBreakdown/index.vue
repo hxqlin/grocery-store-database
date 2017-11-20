5ae36b7b685dc39835f865816e30bee004dba948
hxqlin <hxqlin@gmail.com>
2017-11-19 21:29:33 -0800

    better display

--- demoui/pages/customers/_customerid/_spendingBreakdown/index.vue
+++ demoui/pages/customers/_customerid/_spendingBreakdown/index.vue
@@ -2,15 +2,9 @@
   <section class="user-view">
     <div class="content">
       <div class="subsection">
-<<<<<<< HEAD
         <span class="customer-customerid" style="padding: 10px 0 10px 10px; margin: 10px 0 10px 0;">{{`Spending Breakdown for Customer #${this.$route.params.customerid}` }}</span>
         <li v-for="(total, index) in totals" :key="index" style="padding: 10px 20px; margin: 0 25px; position: relative;">
           <span class="customer-totals" style="padding: 10px 10px; margin: 10px 0 10px 0;">{{ `${total.departmentname + " Total: $" + total.sum}` }}</span><br><br>
-=======
-        <span class="customer-heading" style="padding: 10px 10px; margin: 10px 0 10px 0;">Spending Breakdown:</span><br><br>
-        <li v-for="(total, index) in totals" :key="index" style="padding: 10px 20px; margin: 0 25px; position: relative;">
-          <span class="customer-totals" style="padding: 10px 10px; margin: 10px 0 10px 0;">{{ `${total.departmentname + " Department: $" + total.sum + " total."}` }}</span>
->>>>>>> 8d9f13554a3aa168f27f314e893d626cca76d4c1
         </li>
       </div>
     </div>
@@ -55,6 +49,10 @@ export default {
   padding 10px 30px 10px 30px
   position relative
   line-height 20px
+  .customer-customerid
+    font-size 24px
+    font-weight 500
+    color #707070
   .customer-heading
     font-size 24px
     font-weight 500
